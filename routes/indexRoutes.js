a249b95498630bdf6832efd59eb92c140e6a7b4a
Alvin Martin Djong <alvinmartindwrk@gmail.com>
2021-07-15 21:19:31 +0700

    prepare profile and inventory routes

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -7,4 +7,8 @@ router.get('/', indexController.home)
 
 router.get('/books', indexController.allBooks)
 
+router.get('/inventory', indexController.borrowedBooks)
+
+router.get('/profile', indexController.userProfile)
+
 module.exports = router
